bd7d42129090228dfdf7b55c64d7f1321712de60
rey-holafly <124048302+rey-holafly@users.noreply.github.com>
2024-05-29 15:35:31 -0400

    image cache done

--- OutCodingAssignment/CatApp/View/Template/RemoteImage.swift
+++ OutCodingAssignment/CatApp/View/Template/RemoteImage.swift
@@ -13,6 +13,7 @@ struct RemoteImage: View {
     @State private var image: Image? = nil
     @State private var width: CGFloat
     @State private var height: CGFloat
+    @ObservedObject private var viewModel = CatViewModel() 
     
     init(url: URL, width: CGFloat, height: CGFloat) {
         self.url = url
@@ -21,26 +22,39 @@ struct RemoteImage: View {
     }
     
     var body: some View {
-        if let image = image {
-            image
+        if let cachedImage = viewModel.cachedImage(for: url) {
+            Image(uiImage: cachedImage)
                 .resizable()
                 .scaledToFill()
                 .frame(width: width, height: height)
         } else {
-            ProgressView()
-                .onAppear {
-                    loadImage()
-                }
+            if let image = image {
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: width, height: height)
+            } else {
+                ProgressView()
+                    .onAppear {
+                        loadImage()
+                    }
+            }
         }
     }
     
     private func loadImage() {
-        URLSession.shared.dataTask(with: url) { data, _, _ in
-            if let data = data, let uiImage = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self.image = Image(uiImage: uiImage)
+        if let cachedImage = viewModel.cachedImage(for: url) {
+            self.image = Image(uiImage: cachedImage)
+        } else {
+            URLSession.shared.dataTask(with: url) { data, _, _ in
+                if let data = data, let uiImage = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self.image = Image(uiImage: uiImage)
+                        // Cache the downloaded image
+                        self.viewModel.cacheImages([CatImage(id: "", tags: [])])
+                    }
                 }
-            }
-        }.resume()
+            }.resume()
+        }
     }
 }
