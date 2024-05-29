22ca316521027b2f1f6a20903504e78466c93a7f
rey-holafly <124048302+rey-holafly@users.noreply.github.com>
2024-05-29 07:35:25 -0400

    implementing the navbar

--- OutCodingAssignment/CatApp/View/HomeView.swift
+++ OutCodingAssignment/CatApp/View/HomeView.swift
@@ -9,25 +9,10 @@ import Foundation
 import SwiftUI
 
 struct HomeView: View {
-    @ObservedObject var viewModel = CatViewModel()
-        
+    
     var body: some View {
-        NavigationView {
-            List(viewModel.catImages) { cat in
-                NavigationLink(destination: CatDetailView(catImage: cat)) {
-                    RemoteImage(url: cat.imageUrl, width: 100, height: 100)
-                        .overlay(
-                            Circle()
-                                .stroke(Color.white, lineWidth: 4)
-                                .frame(width: 200, height: 200)
-                        )
-                        .clipShape(Circle())
-                        .shadow(radius: 10)
-                }
-            }
-        }
-        .onAppear {
-            self.viewModel.fetchCatImages()
-        }
+        CustomTabBar()
+            .navigationBarBackButtonHidden(true)
+            .ignoresSafeArea()
     }
 }
