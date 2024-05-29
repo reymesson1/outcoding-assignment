f8c49d683a19f253ed416028d9c57b70420e83af
rey-holafly <124048302+rey-holafly@users.noreply.github.com>
2024-05-29 15:11:37 -0400

    skeleton ready

--- OutCodingAssignment/CatApp/View/CatHomeView.swift
+++ OutCodingAssignment/CatApp/View/CatHomeView.swift
@@ -10,24 +10,50 @@ import SwiftUI
 
 struct CatHomeView: View {
     @ObservedObject var viewModel = CatViewModel()
-        
+    @State private var isLoading = true
+    
     var body: some View {
         NavigationView {
-            List(viewModel.catImages) { cat in
-                NavigationLink(destination: CatDetailView(catImage: cat)) {
-                    RemoteImage(url: cat.imageUrl, width: 100, height: 100)
-                        .overlay(
-                            Circle()
-                                .stroke(Color.white, lineWidth: 4)
-                                .frame(width: 200, height: 200)
-                        )
-                        .clipShape(Circle())
-                        .shadow(radius: 10)
+            if isLoading {
+                SkeletonView()
+            } else {
+                List(viewModel.catImages) { cat in
+                    NavigationLink(destination: CatDetailView(catImage: cat)) {
+                        RemoteImage(url: cat.imageUrl, width: 100, height: 100)
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 4)
+                                    .frame(width: 200, height: 200)
+                            )
+                            .clipShape(Circle())
+                            .shadow(radius: 10)
+                    }
                 }
             }
         }
         .onAppear {
-            self.viewModel.fetchCatImages()
+            self.viewModel.fetchCatImages {
+                self.isLoading = false
+            }
         }
+    }
+}
+
+struct SkeletonView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            ForEach(0..<5) { _ in
+                SkeletonItem()
+            }
+        }
+        .padding()
+    }
+}
+
+struct SkeletonItem: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 8)
+            .foregroundColor(Color.gray.opacity(0.4))
+            .frame(height: 100)
     }
 }
